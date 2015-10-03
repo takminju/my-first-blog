342751bcb8050574e774052d02e09626b8c6390b
takminju <minjo0425@hanmail.net>
2015-10-03 12:38:39 +0900

    My Django Girls app, 첫번째 커밋

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models #해더파일과 같은거 
+from django.utils import timezone
+
+
+class Post(models.Model): #객체 정의 
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
